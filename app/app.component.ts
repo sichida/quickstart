a634487fbab3a4caba8e979bb34f25a85b9d7a8a
Shoun Ichida <shoun@pc45.home>
2016-04-09 15:25:39 +0200

    1ère partie de la soirée NightClazz de Zenika

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,21 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>My First {{projectName}}</h1>
+    <input type="text" [(ngModel)]="projectName" />
+    <input type="text" [ngModel]="projectName" (ngModelChange)="projectName=$event" />
+    <button *ngIf="projectName" (click)="alertMe('my project name is: ' + projectName)">Alert me</button>
+    <button template="ngIf:projectName" (click)="alertMe('my project name is: ' + projectName)">Alert me</button>
+    <template [ngIf]="projectName">
+        <button (click)="alertMe('my project name is: ' + projectName)">Alert me</button>
+    </template>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    private projectName : string = "NightClazz Exo 1";
+    
+    public alertMe(message: string): void {
+        alert(message);
+    }
+ }
